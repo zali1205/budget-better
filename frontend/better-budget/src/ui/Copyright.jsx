1440e7a8c8d3ac178941056c93590a29c16dc591
Zeeshan Ali <zee@Zeeshans-MacBook-Pro.local>
2023-10-22 21:12:07 -0400

    updated copyright

--- frontend/better-budget/src/ui/Copyright.jsx
+++ frontend/better-budget/src/ui/Copyright.jsx
@@ -3,9 +3,9 @@ import { Link as RouterLink } from "react-router-dom";
 
 function Copyright() {
   return (
-    <Typography variant="p" sx={{ my: 6 }}>
+    <Typography variant="p" sx={{ my: 6, color: "text.secondary" }}>
       Copyright ©{" "}
-      <Link component={RouterLink} to="/">
+      <Link component={RouterLink} to="/" color="inherit">
         Better Budget
       </Link>
       {" " + new Date().getFullYear()}
