f937aa19888a1702d04f11df74c001e7faf9c7cd
zali1205 <zali1205@gmail.com>
2023-11-06 20:57:46 -0500

    updated spending history page

--- frontend/better-budget/src/pages/SpendingHistory.jsx
+++ frontend/better-budget/src/pages/SpendingHistory.jsx
@@ -1,17 +1,24 @@
-import { Box, Typography } from "@mui/material";
+import { Box } from "@mui/material";
 import SpendingHistoryOperations from "../features/spending-history/SpendingHistoryOperations";
+import SpendingHistoryTable from "../features/spending-history/SpendingHistoryTable";
 
 function SpendingHistory() {
   return (
-    <Box sx={{ display: "flex", flexDirection: "row", alignItems: "center" }}>
-      <Typography
-        variant="h4"
-        sx={{ flexGrow: 1, display: { xs: "none", sm: "block" } }}
+    <>
+      <Box
+        sx={{
+          display: "flex",
+          flexDirection: "row",
+          alignItems: "center",
+          justifyContent: "flex-end",
+        }}
       >
-        <b>Spending History</b>
-      </Typography>
-      <SpendingHistoryOperations />
-    </Box>
+        <SpendingHistoryOperations />
+      </Box>
+      <Box sx={{ display: "flex", justifyContent: "center", paddingTop: 2 }}>
+        <SpendingHistoryTable />
+      </Box>
+    </>
   );
 }
 
